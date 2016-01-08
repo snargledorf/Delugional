3d035b0d9408169b889a96e36fe6576dd94e619e
Ryan Esteves <snargledorf@gmail.com>
2016-01-07 20:09:29 -0500

    Use WhenableDictionary from the Whenables library in place of WhenList for received messages

--- Delugional/Rpc/DelugeRpc.cs
+++ Delugional/Rpc/DelugeRpc.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Delugional.Utility;
+using Whenables;
 
 namespace Delugional.Rpc
 {
     public interface IDelugeRpc : IDeluge
     {
-        Task<AuthLevels> LoginAsync(string username, string password);
-
         Task<object> Call(string method, params object[] args);
         Task<object> Call(int id, string method, params object[] args);
         Task<object> Call(string method, IDictionary<string, object> kwargs, params object[] args);
@@ -17,13 +16,14 @@ namespace Delugional.Rpc
         Task<object> Call(RpcRequest request);
         Task<object[]> Call(params RpcRequest[] requests);
         Task<object[]> Call(IEnumerable<RpcRequest> requests);
+        Task<AuthLevels> LoginAsync(string username, string password);
     }
 
     public class DelugeRpc : Deluge, IDelugeRpc
     {
         private readonly IDelugeRpcConnection connection;
-        
-        private readonly WhenList<RpcMessage> receivedMessages = new WhenList<RpcMessage>();
+
+        private readonly WhenableDictionary<int, RpcMessage> receivedMessages = new WhenableDictionary<int, RpcMessage>();
 
         public DelugeRpc(IDelugeRpcConnection connection)
         {
@@ -35,14 +35,90 @@ namespace Delugional.Rpc
             BeginReceiving();
         }
 
-        public virtual async Task<AuthLevels> LoginAsync(string username, string password)
+        public override void Close()
         {
-            if (string.IsNullOrWhiteSpace(username))
-                throw new ArgumentException("Argument is null or whitespace", nameof(username));
-            if (string.IsNullOrWhiteSpace(password))
-                throw new ArgumentException("Argument is null or whitespace", nameof(password));
-            
-            return (AuthLevels)await Call("daemon.login", username, password);
+            connection.Close();
+            connection.Dispose();
+        }
+
+        protected RpcResponse CheckResponse(RpcMessage result)
+        {
+            var error = result as RpcError;
+            if (error != null)
+                throw new RpcErrorException(error);
+
+            return (RpcResponse)result;
+        }
+
+        protected IEnumerable<RpcResponse> CheckResponses(RpcMessage[] messages)
+        {
+            var exceptions = new List<Exception>();
+            var responses = new List<RpcResponse>();
+
+            foreach (RpcMessage message in messages)
+            {
+                try
+                {
+                    RpcResponse response = CheckResponse(message);
+                    responses.Add(response);
+                }
+                catch (Exception ex)
+                {
+                    exceptions.Add(ex);
+                }
+            }
+
+            if (exceptions.Any())
+                throw new AggregateException(exceptions);
+
+            return responses;
+        }
+
+        private async void BeginReceiving()
+        {
+            try
+            {
+                while (connection.IsOpen)
+                {
+                    RpcMessage[] messages = await connection.Receive();
+
+                    foreach (RpcMessage message in messages)
+                    {
+                        if (message.Type == MessageType.Response || message.Type == MessageType.Error)
+                        {
+                            receivedMessages.Add(message.Id, message);
+                        }
+                        else if (message.Type == MessageType.Event)
+                        {
+                            // TODO Events
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private Task<RpcMessage> CreateReceiveMessageTask(RpcRequest request)
+        {
+            return receivedMessages.WhenAdded(id => id == request.Id)
+                .GetAsync()
+                .ContinueWith(t =>
+                {
+                    KeyValuePair<int, RpcMessage> result = t.Result;
+                    receivedMessages.Remove(result.Key);
+                    return result.Value;
+                });
+        }
+
+        public override async Task<string> AddMagnetAsync(string url, IDictionary<string, object> options = null)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                throw new ArgumentException("Argument is null or whitespace", nameof(url));
+
+            return await Call("core.add_torrent_magnet", url, options?.ToObjectDictionary()) as string;
         }
 
         public override async Task<string> AddTorrentAsync(string fileName, byte[] fileDump, IDictionary<string, object> options = null)
@@ -57,45 +133,6 @@ namespace Delugional.Rpc
             string fileContents = Base64.Encode(fileDump);
 
             return await Call("core.add_torrent_file", fileName, fileContents, options?.ToObjectDictionary()) as string;
-        }
-
-        public override async Task<string> AddMagnetAsync(string url, IDictionary<string, object> options = null)
-        {
-            if (string.IsNullOrWhiteSpace(url))
-                throw new ArgumentException("Argument is null or whitespace", nameof(url));
-
-            return await Call("core.add_torrent_magnet", url, options?.ToObjectDictionary()) as string;
-        }
-
-        public override async Task<bool> RemoveTorrentAsync(string torrentId, bool removeData = false)
-        {
-            if (string.IsNullOrWhiteSpace(torrentId))
-                throw new ArgumentException("Argument is null or whitespace", nameof(torrentId));
-
-            object result = await Call("core.remove_torrent", torrentId, removeData);
-
-            return result is bool && (bool)result;
-        }
-
-        public override async Task<object[]> RemoveTorrentsAsync(string[] torrentIds, bool removeData = false)
-        {
-            if (torrentIds == null)
-                throw new ArgumentNullException(nameof(torrentIds));
-            if (torrentIds.Length == 0)
-                throw new ArgumentException("Argument is empty collection", nameof(torrentIds));
-
-            return await Call("core.remove_torrents", torrentIds.ToObjectArray(), removeData) as object[];
-        }
-
-        public override async Task<IDictionary<string, object>> GetTorrentStatusAsync(string torrentId, string[] statusKeys = null, bool diff = false)
-        {
-            if (string.IsNullOrWhiteSpace(torrentId))
-                throw new ArgumentException("Argument is null or whitespace", nameof(torrentId));
-
-            object result = await Call("core.get_torrent_status", torrentId, statusKeys.ToObjectArray(), diff);
-
-            var statuses = (Dictionary<object, object>) result;
-            return statuses?.ToDictionary(s => (string)s.Key, s => s.Value);
         }
 
         public override async Task<IDictionary<string, IDictionary<string, object>>> GetTorrentsStatusAsync(Filter filter = null, string[] statusKeys = null, bool diff = false)
@@ -117,6 +154,37 @@ namespace Delugional.Rpc
             }
 
             return dict;
+        }
+
+        public override async Task<IDictionary<string, object>> GetTorrentStatusAsync(string torrentId, string[] statusKeys = null, bool diff = false)
+        {
+            if (string.IsNullOrWhiteSpace(torrentId))
+                throw new ArgumentException("Argument is null or whitespace", nameof(torrentId));
+
+            object result = await Call("core.get_torrent_status", torrentId, statusKeys.ToObjectArray(), diff);
+
+            var statuses = (Dictionary<object, object>) result;
+            return statuses?.ToDictionary(s => (string)s.Key, s => s.Value);
+        }
+
+        public override async Task<bool> RemoveTorrentAsync(string torrentId, bool removeData = false)
+        {
+            if (string.IsNullOrWhiteSpace(torrentId))
+                throw new ArgumentException("Argument is null or whitespace", nameof(torrentId));
+
+            object result = await Call("core.remove_torrent", torrentId, removeData);
+
+            return result is bool && (bool)result;
+        }
+
+        public override async Task<object[]> RemoveTorrentsAsync(string[] torrentIds, bool removeData = false)
+        {
+            if (torrentIds == null)
+                throw new ArgumentNullException(nameof(torrentIds));
+            if (torrentIds.Length == 0)
+                throw new ArgumentException("Argument is empty collection", nameof(torrentIds));
+
+            return await Call("core.remove_torrents", torrentIds.ToObjectArray(), removeData) as object[];
         }
 
         public virtual Task<object> Call(string method, params object[] args)
@@ -174,7 +242,8 @@ namespace Delugional.Rpc
 
             await connection.Send(requests);
 
-            IEnumerable<Task<RpcMessage>> getTasks = requests.Select(request => receivedMessages.GetWhen(m => m.Id == request.Id));
+            IEnumerable<Task<RpcMessage>> getTasks =
+                requests.Select(CreateReceiveMessageTask);
 
             RpcMessage[] messages = await Task.WhenAll(getTasks);
 
@@ -183,70 +252,14 @@ namespace Delugional.Rpc
             return responses.Select(response => response.Data).ToArray();
         }
 
-        public override void Close()
+        public virtual async Task<AuthLevels> LoginAsync(string username, string password)
         {
-            connection.Close();
-            connection.Dispose();
-        }
-
-        protected IEnumerable<RpcResponse> CheckResponses(RpcMessage[] messages)
-        {
-            var exceptions = new List<Exception>();
-            var responses = new List<RpcResponse>();
-
-            foreach (RpcMessage message in messages)
-            {
-                try
-                {
-                    RpcResponse response = CheckResponse(message);
-                    responses.Add(response);
-                }
-                catch (Exception ex)
-                {
-                    exceptions.Add(ex);
-                }
-            }
-
-            if (exceptions.Any())
-                throw new AggregateException(exceptions);
-
-            return responses;
-        }
-
-        protected RpcResponse CheckResponse(RpcMessage result)
-        {
-            var error = result as RpcError;
-            if (error != null)
-                throw new RpcErrorException(error);
-
-            return (RpcResponse)result;
-        }
-
-        private async void BeginReceiving()
-        {
-            try
-            {
-                while (connection.IsOpen)
-                {
-                    RpcMessage[] messages = await connection.Receive();
-
-                    foreach (RpcMessage message in messages)
-                    {
-                        if (message.Type == MessageType.Response || message.Type == MessageType.Error)
-                        {
-                            receivedMessages.Add(message);
-                        }
-                        else if (message.Type == MessageType.Event)
-                        {
-                            // TODO Events
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentException("Argument is null or whitespace", nameof(username));
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentException("Argument is null or whitespace", nameof(password));
+            
+            return (AuthLevels)await Call("daemon.login", username, password);
         }
     }
 }
